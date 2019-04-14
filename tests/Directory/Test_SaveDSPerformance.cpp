cdcd906bac770d5ba3e4f3a0ad9eadcc5efe3a32
amon <amon@nandynarwhals.org>
2019-04-14 22:51:04 +0800

    .

--- tests/Directory/Test_SaveDSPerformance.cpp
+++ tests/Directory/Test_SaveDSPerformance.cpp
@@ -41,14 +41,73 @@
 #define DS_DIFF 1
 #define SHARD_DIFF 1
 #define GAS_PRICE 1
+#define NUM_OF_FINAL_BLOCK 100
+#define FINALBLOCK_REWARD -1
 
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE(savedsperformance)
 
-// Test the original behaviour: nodes expire by having their index incremented
-// above the DS committee size.
-BOOST_FIXTURE_TEST_CASE(test_UpdateWithoutRemovals, F) {
+struct F {
+  F() {
+    BOOST_TEST_MESSAGE("setup fixture");
+
+    // Generate the self key.
+    selfKeyPair = Schnorr::GetInstance().GenKeyPair();
+    selfPubKey = selfKeyPair.second;
+
+    // Generate the DS Committee.
+    for (int i = 0; i < COMMITTEE_SIZE; ++i) {
+      PairOfKey kp = Schnorr::GetInstance().GenKeyPair();
+      PubKey pk = kp.second;
+      Peer peer = Peer(LOCALHOST, BASE_PORT + i);
+      PairOfNode entry = std::make_pair(pk, peer);
+      dsComm.emplace_back(entry);
+    }
+  }
+
+  ~F() { BOOST_TEST_MESSAGE("teardown fixture"); }
+
+  PairOfKey selfKeyPair;
+  PubKey selfPubKey;
+  DequeOfNode dsComm;
+};
+
+// Test that no previous performance data is carried over.
+
+BOOST_FIXTURE_TEST_CASE(test_CleanSave, F) {
+  INIT_STDOUT_LOGGER();
+
+  // Create an empty coinbase rewards.
+  std::map<uint64_t, std::map<int32_t, std::vector<PubKey>>> coinbaseRewardees;
+
+  // Create some previous data in the member performance.
+  std::map<PubKey, uint32_t> dsMemberPerformance;
+  for (const auto& member : dsComm) {
+    dsMemberPerformance[member.first] = 10;
+  }
+
+  InternalSaveDSPerformance(coinbaseRewardees, dsMemberPerformance, dsComm,
+                            EPOCH_NUM, NUM_OF_FINAL_BLOCK, FINALBLOCK_REWARD);
+
+  // Check the size.
+  BOOST_CHECK_MESSAGE(dsMemberPerformance.size() == COMMITTEE_SIZE,
+                      "Expected DS Performance size wrong. Actual: "
+                          << dsMemberPerformance.size()
+                          << ". Expected: " << COMMITTEE_SIZE);
+
+  // Check the result.
+  for (const auto& member : dsComm) {
+    BOOST_CHECK_MESSAGE(dsMemberPerformance.at(member.first) != 0,
+                        "Pub Key " << member.first
+                                   << " is not cleared. Expected: 0. Result: "
+                                   << dsMemberPerformance.at(member.first));
+  }
+}
+
+// Test the legitimate case.
+
+BOOST_FIXTURE_TEST_CASE(test_LegitimateCase, F) {
   INIT_STDOUT_LOGGER();
 
   // Create the winners.
@@ -80,123 +139,6 @@ BOOST_FIXTURE_TEST_CASE(test_UpdateWithoutRemovals, F) {
   }
   // Shift the existing members less NUM_OF_ELECTED to the back of the deque.
   for (int i = 0; i < (COMMITTEE_SIZE - NUM_OF_ELECTED); ++i) {
-    expectedDSComm.emplace_back(dsComm.at(i));
-  }
-
-  // Check expected commmitee size.
-  BOOST_CHECK_MESSAGE(expectedDSComm.size() == COMMITTEE_SIZE,
-                      "Expected DS Committee size wrong. Actual: "
-                          << expectedDSComm.size()
-                          << ". Expected: " << COMMITTEE_SIZE);
-
-  // Update the DS Composition.
-  InternalUpdateDSCommitteeComposition(selfPubKey, dsComm, block);
-
-  // Check updated commmitee size.
-  BOOST_CHECK_MESSAGE(dsComm.size() == COMMITTEE_SIZE,
-                      "Updated DS Committee size wrong. Actual: "
-                          << dsComm.size() << ". Expected: " << COMMITTEE_SIZE);
-
-  // Check the result.
-  for (int i = 0; i < COMMITTEE_SIZE; ++i) {
-    // Compare the public keys.
-    PubKey actual = dsComm.at(i).first;
-    PubKey expected = expectedDSComm.at(i).first;
-    BOOST_CHECK_MESSAGE(
-        actual == expected,
-        "Index: " << i << ". Expected: " << expected << ". Result: " << actual);
-  }
-}
-
-// Test that the composition does not change when the winners is empty.
-BOOST_FIXTURE_TEST_CASE(test_UpdateWithoutWinners, F) {
-  INIT_STDOUT_LOGGER();
-
-  // Create the empty winners map.
-  std::map<PubKey, Peer> winners;
-
-  // Creat the empty nodes to be removed vector.
-  std::vector<PubKey> removeDSNodePubkeys;
-
-  // Construct the fake DS Block.
-  PairOfKey leaderKeyPair = Schnorr::GetInstance().GenKeyPair();
-  PubKey leaderPubKey = leaderKeyPair.second;
-  DSBlockHeader header(DS_DIFF, SHARD_DIFF, leaderPubKey, BLOCK_NUM, EPOCH_NUM,
-                       GAS_PRICE, SWInfo(), winners, removeDSNodePubkeys,
-                       DSBlockHashSet());
-  DSBlock block(header, CoSignatures());
-
-  // Build the expected composition.
-  DequeOfNode expectedDSComm;
-  // Copy the existing members.
-  for (int i = 0; i < COMMITTEE_SIZE; ++i) {
-    expectedDSComm.emplace_back(dsComm.at(i));
-  }
-
-  // Check expected commmitee size.
-  BOOST_CHECK_MESSAGE(expectedDSComm.size() == COMMITTEE_SIZE,
-                      "Expected DS Committee size wrong. Actual: "
-                          << expectedDSComm.size()
-                          << ". Expected: " << COMMITTEE_SIZE);
-
-  // Update the DS Composition.
-  InternalUpdateDSCommitteeComposition(selfPubKey, dsComm, block);
-
-  // Check updated commmitee size.
-  BOOST_CHECK_MESSAGE(dsComm.size() == COMMITTEE_SIZE,
-                      "Updated DS Committee size wrong. Actual: "
-                          << dsComm.size() << ". Expected: " << COMMITTEE_SIZE);
-
-  // Check the result.
-  for (int i = 0; i < COMMITTEE_SIZE; ++i) {
-    // Compare the public keys.
-    PubKey actual = dsComm.at(i).first;
-    PubKey expected = expectedDSComm.at(i).first;
-    BOOST_CHECK_MESSAGE(
-        actual == expected,
-        "Index: " << i << ". Expected: " << expected << ". Result: " << actual);
-  }
-}
-
-// Test the new behaviour: remove Byzantine nodes.
-BOOST_FIXTURE_TEST_CASE(test_UpdateWithRemovals, F) {
-  INIT_STDOUT_LOGGER();
-
-  // Create the winners.
-  std::map<PubKey, Peer> winners;
-  for (int i = 0; i < NUM_OF_ELECTED; ++i) {
-    PairOfKey candidateKeyPair = Schnorr::GetInstance().GenKeyPair();
-    PubKey candidatePubKey = candidateKeyPair.second;
-    Peer candidatePeer = Peer(LOCALHOST, BASE_PORT + COMMITTEE_SIZE + i);
-    winners[candidatePubKey] = candidatePeer;
-  }
-
-  // Create the removed members.
-  std::vector<PubKey> removeDSNodePubkeys;
-  for (int i = 0; i < NUM_OF_REMOVED; ++i) {
-    PairOfNode kp = dsComm.at(i);
-    removeDSNodePubkeys.emplace_back(kp.first);
-  }
-
-  // Construct the fake DS Block.
-  PairOfKey leaderKeyPair = Schnorr::GetInstance().GenKeyPair();
-  PubKey leaderPubKey = leaderKeyPair.second;
-  DSBlockHeader header(DS_DIFF, SHARD_DIFF, leaderPubKey, BLOCK_NUM, EPOCH_NUM,
-                       GAS_PRICE, SWInfo(), winners, removeDSNodePubkeys,
-                       DSBlockHashSet());
-  DSBlock block(header, CoSignatures());
-
-  // Build the expected composition.
-  DequeOfNode expectedDSComm;
-  // Put the winners in front.
-  for (const auto& i : winners) {
-    // New additions are always placed at the beginning.
-    expectedDSComm.emplace_front(i);
-  }
-  // Shift the existing non-removed and non-expired members to the back of the
-  // deque.
-  for (int i = NUM_OF_REMOVED;
-       i < (COMMITTEE_SIZE - (NUM_OF_ELECTED - NUM_OF_REMOVED)); ++i) {
     expectedDSComm.emplace_back(dsComm.at(i));
   }
 
