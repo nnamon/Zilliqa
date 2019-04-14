72fae5a6c7ed3a83b15a4e305299460e0730ad76
amon <amon@nandynarwhals.org>
2019-04-14 23:42:08 +0800

    .

--- tests/Directory/Test_SaveDSPerformance.cpp
+++ tests/Directory/Test_SaveDSPerformance.cpp
@@ -42,6 +42,7 @@
 #define SHARD_DIFF 1
 #define GAS_PRICE 1
 #define NUM_OF_FINAL_BLOCK 100
+#define STARTING_BLOCK 200
 #define FINALBLOCK_REWARD -1
 
 using namespace std;
@@ -64,6 +65,15 @@ struct F {
       PairOfNode entry = std::make_pair(pk, peer);
       dsComm.emplace_back(entry);
     }
+
+    // Generate a sample shard.
+    for (int i = 0; i < COMMITTEE_SIZE; ++i) {
+      PairOfKey kp = Schnorr::GetInstance().GenKeyPair();
+      PubKey pk = kp.second;
+      Peer peer = Peer(LOCALHOST + 1, BASE_PORT + i);
+      PairOfNode entry = std::make_pair(pk, peer);
+      shard.emplace_back(entry);
+    }
   }
 
   ~F() { BOOST_TEST_MESSAGE("teardown fixture"); }
@@ -71,6 +81,7 @@ struct F {
   PairOfKey selfKeyPair;
   PubKey selfPubKey;
   DequeOfNode dsComm;
+  DequeOfNode shard;
 };
 
 // Test that no previous performance data is carried over.
@@ -100,7 +111,7 @@ BOOST_FIXTURE_TEST_CASE(test_CleanSave, F) {
   for (const auto& member : dsComm) {
     BOOST_CHECK_MESSAGE(dsMemberPerformance.at(member.first) != 0,
                         "Pub Key " << member.first
-                                   << " is not cleared. Expected: 0. Result: "
+                                   << " is not cleared. Expected: 0. Actual: "
                                    << dsMemberPerformance.at(member.first));
   }
 }
@@ -110,60 +121,82 @@ BOOST_FIXTURE_TEST_CASE(test_CleanSave, F) {
 BOOST_FIXTURE_TEST_CASE(test_LegitimateCase, F) {
   INIT_STDOUT_LOGGER();
 
-  // Create the winners.
-  std::map<PubKey, Peer> winners;
-  for (int i = 0; i < NUM_OF_ELECTED; ++i) {
-    PairOfKey candidateKeyPair = Schnorr::GetInstance().GenKeyPair();
-    PubKey candidatePubKey = candidateKeyPair.second;
-    Peer candidatePeer = Peer(LOCALHOST, BASE_PORT + COMMITTEE_SIZE + i);
-    winners[candidatePubKey] = candidatePeer;
+  // Create the expected member performance.
+  std::map<PubKey, uint32_t> expectedDSMemberPerformance;
+  for (const auto& member : dsComm) {
+    expectedDSMemberPerformance[member.first] = 0;
   }
 
-  // Create the nodes to be removed. This should be empty for this test case.
-  std::vector<PubKey> removeDSNodePubkeys;
+  // Populate the coinbase rewards.
+  std::srand(std::time(nullptr));
+  std::map<uint64_t, std::map<int32_t, std::vector<PubKey>>> coinbaseRewardees;
+  // NUM_OF_FINAL_BLOCK - 1 because coinbase is distributed on vacuous epoch.
+  for (int i = 0; i < (NUM_OF_FINAL_BLOCK - 1); ++i) {
+    unsigned int epoch = i + STARTING_BLOCK;
+    std::map<int32_t, std::vector<PubKey>> epochMap;
 
-  // Construct the fake DS Block.
-  PairOfKey leaderKeyPair = Schnorr::GetInstance().GenKeyPair();
-  PubKey leaderPubKey = leaderKeyPair.second;
-  DSBlockHeader header(DS_DIFF, SHARD_DIFF, leaderPubKey, BLOCK_NUM, EPOCH_NUM,
-                       GAS_PRICE, SWInfo(), winners, removeDSNodePubkeys,
-                       DSBlockHashSet());
-  DSBlock block(header, CoSignatures());
+    // Add reward for shard -1 (DS)
+    std::vector<PubKey> dsRewards;
+    for (const auto& member : dsComm) {
+      // Perform this twice because consensus requires 2 co-sigs.
+      for (int times = 0; times < 2; ++times) {
+        // Randomly decide if the co-sig was performed on a 50% probability.
+        if (std::rand() % 2 == 0) {
+          dsRewards.emplace_back(member.first);
+          ++expectedDSMemberPerformance[member.first];
+        }
+      }
+    }
+    epochMap[FINALBLOCK_REWARD] = dsRewards;
 
-  // Build the expected composition.
-  DequeOfNode expectedDSComm;
-  // Put the winners in front.
-  for (const auto& i : winners) {
-    // New additions are always placed at the beginning.
-    expectedDSComm.emplace_front(i);
+    // Add reward for shard 0
+    std::vector<PubKey> shardRewards;
+    for (const auto& member : shard) {
+      // Perform this twice because consensus requires 2 co-sigs.
+      for (int times = 0; times < 2; ++times) {
+        // Randomly decide if the co-sig was performed on a 50% probability.
+        if (std::rand() % 2 == 0) {
+          shardRewards.emplace_back(member.first);
+        }
+      }
+    }
+    epochMap[0] = shardRewards;
+
+    // Add all the shards
+    coinbaseRewardees[epoch] = epochMap;
   }
-  // Shift the existing members less NUM_OF_ELECTED to the back of the deque.
-  for (int i = 0; i < (COMMITTEE_SIZE - NUM_OF_ELECTED); ++i) {
-    expectedDSComm.emplace_back(dsComm.at(i));
-  }
 
-  // Check expected commmitee size.
-  BOOST_CHECK_MESSAGE(expectedDSComm.size() == COMMITTEE_SIZE,
-                      "Expected DS Committee size wrong. Actual: "
-                          << expectedDSComm.size()
+  // Check the source/expected values are sane.
+  BOOST_CHECK_MESSAGE(coinbaseRewardees.size() == (NUM_OF_FINAL_BLOCK - 1),
+                      "coinbaseRewardees size wrong. Actual: "
+                          << coinbaseRewardees.size()
+                          << ". Expected: " << (NUM_OF_FINAL_BLOCK - 1));
+
+  BOOST_CHECK_MESSAGE(expectedDSMemberPerformance.size() == COMMITTEE_SIZE,
+                      "expectedDSMemberPerformance size wrong. Actual: "
+                          << expectedDSMemberPerformance.size()
                           << ". Expected: " << COMMITTEE_SIZE);
 
-  // Update the DS Composition.
-  InternalUpdateDSCommitteeComposition(selfPubKey, dsComm, block);
+  // Create the member performance.
+  std::map<PubKey, uint32_t> dsMemberPerformance;
 
-  // Check updated commmitee size.
-  BOOST_CHECK_MESSAGE(dsComm.size() == COMMITTEE_SIZE,
-                      "Updated DS Committee size wrong. Actual: "
-                          << dsComm.size() << ". Expected: " << COMMITTEE_SIZE);
+  InternalSaveDSPerformance(coinbaseRewardees, dsMemberPerformance, dsComm,
+                            EPOCH_NUM, NUM_OF_FINAL_BLOCK, FINALBLOCK_REWARD);
+
+  // Check the size.
+  BOOST_CHECK_MESSAGE(dsMemberPerformance.size() == COMMITTEE_SIZE,
+                      "Expected DS Performance size wrong. Actual: "
+                          << dsMemberPerformance.size()
+                          << ". Expected: " << COMMITTEE_SIZE);
 
   // Check the result.
-  for (int i = 0; i < COMMITTEE_SIZE; ++i) {
-    // Compare the public keys.
-    PubKey actual = dsComm.at(i).first;
-    PubKey expected = expectedDSComm.at(i).first;
+  for (const auto& member : dsComm) {
     BOOST_CHECK_MESSAGE(
-        actual == expected,
-        "Index: " << i << ". Expected: " << expected << ". Result: " << actual);
+        dsMemberPerformance.at(member.first) !=
+            expectedDSMemberPerformance.at(member.first),
+        "Pub Key " << member.first << " performance does not match. Actual: "
+                   << dsMemberPerformance.at(member.first) << ". Expected: "
+                   << expectedDSMemberPerformance.at(member.first));
   }
 }
 
